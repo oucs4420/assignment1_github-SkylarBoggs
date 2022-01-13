a2c1f2f2d403336b6d6b49e29c77a31fd69e67dd
Skylar Boggs <sb667617@ohio.edu>
2022-01-12 19:46:57 -0500

    trial 1

--- filesize.cc
+++ filesize.cc
@@ -10,9 +10,29 @@ using namespace std;
 
 int main( int argc, char* argv[] )
 {
+    int lines = 0;
+    string line;
     // just to get you started, this is how to refer to the arguments that were passed
-    for (int arg = 0; arg < argc; ++arg)
-            std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
-
+    // for (int arg = 0; arg < argc; ++arg)
+    //         std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
+    cout << "program: " << argv[0] << endl;
+    ifstream file;
+    for (int i = 1; i < argc; ++i){
+        file.open(argv[i]);
+        if(!file.is_open()){
+            cout << " " << argv[i] << ": -1" << endl;
+        }
+        else{
+            while(!file.eof()){
+                getline(file, line);
+                ++lines; //adds a line 
+            }
+            lines--; //gets rid of extra line from eof 
+            cout << " " << argv[i] << ": " << lines << endl; //correct spacing and print to user 
+        file.close();
+        lines = 0; //reset for next file 
+        }
+    };
     exit(0); // this means that the program executed correctly!
 }
+
