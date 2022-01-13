42991accec9c453597e26b2b9ba7eba913a0db43
Skylar Boggs <sb667617@ohio.edu>
2022-01-13 16:41:06 -0500

    Trial 3

--- filesize.cc
+++ filesize.cc
@@ -10,28 +10,24 @@ using namespace std;
 
 int main( int argc, char* argv[] )
 {
-    int lines = 0;
-    string line;
-    // just to get you started, this is how to refer to the arguments that were passed
-    // for (int arg = 0; arg < argc; ++arg)
-    //         std::cout << "argv[" << arg << "]: " << argv[arg] << '\n' ;
-    cout << "program: " << argv[0] << endl;
-    ifstream file;
+    int lines = 0; //counting for total line # 
+    string line = " ";
+    cout << "program: " << argv[0] << endl; //prog name 
+    ifstream file; //file based on arguments 
     for (int i = 1; i < argc; ++i){
-        file.open(argv[i]);
-        if(!file.is_open()){
-            cout << " " << argv[i] << ": -1" << endl;
-        }
+        file.open(argv[i]); //opens files one by one until all arguments gone through 
+        if(!file.is_open()){ //failed to open, error count message 
+            cout << " " << argv[i] << ": -1" << endl; //error 
+        }//end of if 
         else{
             while(file.peek() != EOF){ //Resolves issue w/eof running one more time for some files 
-                getline(file, line);
-                ++lines; //adds a line 
-            }
+                getline(file, line); //reads each lines
+                ++lines; //counts total lines in file i 
+            } //end of while 
             cout << " " << argv[i] << ": " << lines << endl; //correct spacing and print to user 
-        file.close();
+        file.close(); //closes file i 
         lines = 0; //reset for next file 
-        }
-    };
+        }//end of else 
+    };//end of for 
     exit(0); // this means that the program executed correctly!
-}
-
+}//end of main  
